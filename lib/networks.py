76e4fd5977abc27ddb59a82d8f5ed23927e1e5e1
YianniR <yiannis.rav@gmail.com>
2018-04-29 18:23:28 +0100

    Added preliminary batch maker function for rnn

--- lib/networks.py
+++ lib/networks.py
@@ -2,20 +2,29 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.contrib import rnn
 
-def set_up_placeholders(n_steps,n_inputs,n_outputs):
+def set_up_placeholders(n_inputs,n_steps,n_outputs):
 
     tf.reset_default_graph()
 
     # Height by Width
-    input_ = tf.placeholder('float',[None,n_inputs])
-    target_ = tf.placeholder('float',[None,n_outputs])
+    input_ = tf.placeholder('float',[None,n_steps,n_inputs],name='input')
+    target_ = tf.placeholder('float',[None,n_steps,n_outputs],name='target')
 
     return input_,target_
 
-def rnn_model(input_,rnn_size,n_classes):
-    basic_cell = rnn.BasicRNNCell(num_units=rnn_size)
-    outputs, states = tf.nn.dynamic_rnn(basic_cell, input_, dtype=tf.float32)
-    logits = tf.layers.dense(states, n_classes)
+def rnn_model(input_,rnn_size,n_outputs):
+
+    # RNN output node weights and biases
+    weights = {
+        'out': tf.Variable(tf.random_normal([rnn_size, n_outputs]))
+    }
+    biases = {
+        'out': tf.Variable(tf.random_normal([n_outputs]))
+    }
+
+    cell = tf.contrib.rnn.BasicRNNCell(num_units=rnn_size, activation=tf.nn.relu) #hiddel layer for 1 time step
+    outputs, states = tf.nn.dynamic_rnn(cell, input_, dtype=tf.float32) # Use hidden layer for n_steps timesteps
+    logits = tf.layers.dense(outputs, n_outputs, name='predictions')
 
     return logits
 
@@ -46,29 +55,56 @@ def multilayer_perceptron_model(input_,n_input_nodes,n_nodes_hl1,n_nodes_hl2,n_n
 
     return output
 
-def train(x,y,logits,train_x, train_y, test_x, test_y,batch_size,num_epochs,learning_rate=0.001):
-    xentropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels=y)
+def batch_maker(train_x,train_y,n_steps,batch_size):
+    i=0
+    batches_x = []
+    batches_y = []
+    while i < len(train_x):
+
+        j=0
+        step_x = []
+        step_y = []
+        while j < batch_size:
+            step_start = j;
+            step_end = j + n_steps
+
+            step_x.append(train_x[step_start:step_end])
+            step_y.append(train_y[step_start:step_end])
+
+            j += n_steps
+
+        batches_x.append(step_x)
+        batches_y.append(step_x)
+        i += batch_size
+
+    return batches_x,batches_y
+
+def train(x,y,logits,train_x, train_y, test_x, test_y,n_steps,batch_size,num_epochs,learning_rate=0.001):
+    xentropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits,labels=y)
     cost = tf.reduce_mean(xentropy)
     optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
+        print("Making Batches")
+        batches_x,batches_y = batch_maker(train_x,train_y,n_steps,batch_size)
+
+        i = 0
         for epoch in range(num_epochs):
+            print('Epoch', epoch, ' started out of', num_epochs)
             epoch_loss = 0
 
-            i = 0;
-            while i < len(train_x):
-                start = i;
-                end = i + batch_size
-
-                batch_x = np.array(train_x[start:end])
-                batch_y = np.array(train_y[start:end])
+            n_batches = len(train_x)/batch_size
+            while i < n_batches:
+                print('Batch', i, ' started out of', n_batches)
+                batch_x = batches_x[i]
+                batch_y = batches_y[i]
 
                 _, c = sess.run([optimizer,cost],feed_dict = {x: batch_x,y: batch_y})
                 epoch_loss += c
 
-                i += batch_size
+                i += 1
 
             print('Epoch', epoch, 'completed out of', num_epochs, 'loss:',epoch_loss)
 
--- run.py
+++ run.py
@@ -8,21 +8,22 @@ from pprint import pprint
 def main():
 
     #Set up parameters (can put those in a config file and parse them.)
+    batch_size = 30
     n_steps   = 10
     n_inputs  = 127
+    n_neurons = 150
     n_outputs = 127
     learning_rate = 0.001
-    batch_size = 150
     num_epochs = 200
 
     #Set up placeholders
-    input_,target_ = set_up_placeholders(n_steps,n_inputs,n_outputs)
+    input_,target_ = set_up_placeholders(n_inputs,n_steps,n_outputs)
 
     #Set up dataset
     data = Dataset("Dataset 1","bach","Just the one hot vectors, with no pre-processing","28/5/2018")
     # data.make_dataset(trainingSplit=0.9)
     # data.save("dataset1.pickle")
-    data.load("dataset1.pickle")
+    data.load("bach.pickle")
 
     #Split input and targets. (I'll make a function to do that, also idk if this is the right way)
     train_x = data.train[0:-1]
@@ -30,12 +31,15 @@ def main():
     test_x = data.test[0:-1]
     test_y = data.test[1:]
 
+
+
     print("Train Length:" + str(len(data.train)))
     print("Test Length :" + str(len(data.test)))
 
     # #Setup model and train it
-    prediction = multilayer_perceptron_model(input_,n_inputs,150,100,75,n_outputs)
-    train(input_,target_,prediction,train_x, train_y, test_x, test_y,batch_size,num_epochs)
+    #prediction = multilayer_perceptron_model(input_,n_inputs,150,100,75,n_outputs)
+    prediction = rnn_model(input_,n_steps,n_outputs)
+    train(input_,target_,prediction,train_x, train_y, test_x, test_y,n_steps,batch_size,num_epochs)
 
 if __name__ == "__main__":
     main()
